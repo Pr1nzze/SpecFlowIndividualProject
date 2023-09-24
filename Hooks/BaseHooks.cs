1b3008e8772635e7a82aa2201c1700ae9f4b4870
pr1nzze <prinzze@hotmail.com>
2023-09-24 21:47:10 +0100

    ...

--- Hooks/BaseHooks.cs
+++ Hooks/BaseHooks.cs
@@ -18,10 +18,7 @@ namespace SpecFlowIndividualProject.NewFolder
         public void BeforeScenarioWithTag()
         {
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("start-maximized", "incognito");
-            options.AddArguments(" - - no- sandbox");
-            options.AddArguments(" - -disable-dev-shm-usage ");
-            options.AddArgument(" - -headless ");
+            options.AddArguments("start-maximized", "incognito", "--no-sandbox", "--disable-dev-shm-usage", "--headless");
             Driver = new ChromeDriver(options);
             container.RegisterInstanceAs(Driver);
         }
